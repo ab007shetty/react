6ba789c65ec3e4f150a29ec49809569fa205b3db
Anirudha B Shetty <45870174+ab007shetty@users.noreply.github.com>
2020-09-01 16:52:06 +0530

    React Router

--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -2,11 +2,7 @@ import React, {Component} from 'react';
 import {Card, CardImg,  CardImgOverlay, CardTitle} from 'reactstrap';
 
 class Menu extends Component{
-    
-    constructor(props) {
-        super(props);    
-    }
-
+  
     render() {
 
         const menu = this.props.dishes.map((dish)=> {
