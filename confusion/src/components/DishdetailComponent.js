8442feef76a96878e87244858e45268e8ad2b34b
Anirudha B Shetty <45870174+ab007shetty@users.noreply.github.com>
2020-09-02 11:33:50 +0530

    Assignment2

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody,CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
